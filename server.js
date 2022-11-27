a1040f2b656c937505428fa512186d3117f72376
Kana Taguchi <kana.taguchi@relic.co.jp>
2022-11-27 13:34:22 +0900

    Initial Express setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const PORT = process.env.PORT || 5500;
+
+const app = express();
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
