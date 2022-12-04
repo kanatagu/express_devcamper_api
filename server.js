6f61fbfff358f116e48c1cc4cb7edba5217436cc
Kana Taguchi <kana.taguchi@relic.co.jp>
2022-12-04 18:20:53 +0900

    Create custom error handling and Mongoose middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -27,6 +28,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount  routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler)
 
 const server = app.listen(
   PORT,
