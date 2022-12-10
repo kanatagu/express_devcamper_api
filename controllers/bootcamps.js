6d81d23be314fd2bc417900d87a117059f8642ef
Kana Taguchi <kana.taguchi@relic.co.jp>
2022-12-10 19:04:09 +0900

    Create seeder file

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,9 +2,11 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc     Get all bootcamps
-// @route    GET /api/v1/bootcamps
-// @access   Public
+/**
+ * @desc     Get all bootcamps
+ * @route    GET /api/v1/bootcamps
+ * @access   Public
+ */
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const bootcamps = await Bootcamp.find();
   res
@@ -12,9 +14,11 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
 
-// @desc     Get single bootcamps
-// @route    GET /api/v1/bootcamps/:id
-// @access   Public
+/**
+ * @desc     Get single bootcamps
+ * @route    GET /api/v1/bootcamps/:id
+ * @access   Public
+ */
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
 
@@ -29,17 +33,21 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc     Create new bootcamp
-// @route    POST /api/v1/bootcamps
-// @access   Private
+/**
+ * @desc     Create new bootcamp
+ * @route    POST /api/v1/bootcamps
+ * @access   Private
+ */
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({ success: true, data: bootcamp });
 });
 
-// @desc     Update bootcamp
-// @route    PUT /api/v1/bootcamps/:id
-// @access   Private
+/**
+ * @desc     Update bootcamp
+ * @route    PUT /api/v1/bootcamps/:id
+ * @access   Private
+ */
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -55,9 +63,11 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: bootcamp });
 });
 
-// @desc     Delete bootcamp
-// @route    DELETE /api/v1/bootcamps/:id
-// @access   Private
+/**
+ *  @desc    Delete bootcamp
+ * @route    DELETE /api/v1/bootcamps/:id
+ * @access   Private
+ */
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI);
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  // means "node seeder -i"
+  importData();
+} else if (process.argv[2] === '-d') {
+  // means "node seeder -d"
+  deleteData();
+}
